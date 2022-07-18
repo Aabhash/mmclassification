7153996f8fae54aaf5113a0736bd5d4a17c3fb4f
Till Fetzer <till.fetzer@googlemail.com>
2022-07-18 17:17:04 +0200

    config changes

--- configs/cascading/cascading.py
+++ configs/cascading/cascading.py
@@ -1,4 +1,4 @@
 _base_ = [
-    '../_base_/models/dynamic/cascading.py', '../_base_/datasets/cifar10_bs16.py',
+    '../_base_/models/dynamic/skipNet.py', '../_base_/datasets/cifar10_bs16.py',
     '../_base_/schedules/cifar10_bs128.py', '../_base_/default_runtime.py'
 ]
