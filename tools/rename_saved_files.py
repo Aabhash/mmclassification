acf41d7d8f503db4d6cad5242b93b670aa23857c
Till Fetzer <till.fetzer@googlemail.com>
2022-07-24 11:06:47 +0200

    imagenette

--- tools/rename_saved_files.py
+++ tools/rename_saved_files.py
@@ -5,11 +5,11 @@ from collections import OrderedDict
 
 
 def main(
-    file_1 = "/home/till/mmclassification/pretrained_models/resnet18_b16x8_cifar10_20210528-bd6371c8.pth",
-    file_2 = "/home/till/mmclassification/pretrained_models/resnet101_b16x8_cifar10_20210528-2d29e936.pth",
+    file_1 = "/home/till/mmclassification/pretrained_models/resnet50_imagenette.pth",
+    file_2 = "/home/till/mmclassification/work_dirs/resnet18_selftrained/epoch_195.pth",
     renames_1 = ["little", "little_head"],
     renames_2 = ["big", "big_head"],
-    save_new = "/home/till/mmclassification/pretrained_models/cascading_resnet18_resnet_101.pth"
+    save_new = "/home/till/mmclassification/pretrained_models/cascading_resnet18_resnet_50_imagenette.pth"
 ):
     model_1 = torch.load(file_1)
     save_mode = model_1
